dc15f1592caf4e006e886ae3b50dce214a9f182b
Mohammed Janeb <mohammedjaneb92@gmail.com>
2025-01-29 21:59:48 +0300

    test 4

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,30 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "./auth";
 
-// const privateRoutes = ["/calendar", "/my-tasks", "/notification", "/settings"]
+const privateRoutes = ["/calendar", "/my-tasks", "/notification", "/settings"]
 
-// export default auth((req) => {
-//     const { nextUrl, headers } = req;
-//     const originUrl = `${nextUrl.protocol}//${headers.get("host")}`;
+export default auth((req) => {
+    const { nextUrl, headers } = req;
+    const originUrl = `${nextUrl.protocol}//${headers.get("host")}`;
 
-//     const isLoggedIn = !!req.auth; 
-//     const isAuthRoute = nextUrl.pathname.includes("/auth");
-//     const isPrivateRoute = nextUrl.pathname === "/" || privateRoutes.includes(nextUrl.pathname);
+    const isLoggedIn = !!req.auth; 
+    const isAuthRoute = nextUrl.pathname.includes("/auth");
+    const isPrivateRoute = nextUrl.pathname === "/" || privateRoutes.includes(nextUrl.pathname);
     
 
-//     if(isAuthRoute && isLoggedIn) {
-//         return Response.redirect(`${originUrl}`);
-//     }
+    if(isAuthRoute && isLoggedIn) {
+        return Response.redirect(`${originUrl}`);
+    }
 
-//     if(!isLoggedIn && isPrivateRoute) {
-//         return Response.redirect(`${originUrl}/auth/sign-in`)
-//     }
+    if(!isLoggedIn && isPrivateRoute) {
+        return Response.redirect(`${originUrl}/auth/sign-in`)
+    }
 
-// })
+})
 
-// export const config = {
-//     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// }
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    unstable_allowDynamic: [
+        '/lib/mongoose.ts', // allows a single file
+        '**/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
+      ],
+}
